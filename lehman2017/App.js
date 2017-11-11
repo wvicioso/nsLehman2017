b4f3bb2fd863f0b71407c40b81be32fa1f7fc167
wvicioso <wellington.vicioso@gmail.com>
2017-11-11 05:28:24 -0500

    add icon

--- lehman2017/App.js
+++ lehman2017/App.js
@@ -6,79 +6,105 @@ import {
   TouchableOpacity,
   View,
   Text,
-  DeviceEventEmitter
+  Dimensions,
+  Image
 } from 'react-native';
+
+const {width, height} = Dimensions
 
 import Speech from 'react-native-speech';
 import WS from 'react-native-websocket';
 
-
 export default class App extends Component<{}> {
-  // clientAddr;
-  // messagecount = 1;
-  // port = 8080;
 
   constructor() {
     super();
     this.state = {
-      dataFromAlienComp: 'nothing recieved'
+      data: 'No Detection',
+      connected: true,
+      chatter: []
     }
   }
 
-  convertToSpeech(data){
-    Speech.speak({
-      text: 'Car',
-      voice: 'en-US'
+  updateChatter(msg) {
+    this.setState({
+        chatter: this.state.chatter.concat([msg])
     });
   }
 
+  convertToSpeech(resp){
+    if (resp != undefined && resp.type != 'error') {
+      Speech.speak({
+        text: resp.data,
+        voice: 'en-US'
+      });
 
-
+      this.setState({
+        data: resp.data
+      })
+    }
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.button}>
-          <Text>{this.state.dataFromAlienComp}</Text>
-        </TouchableOpacity>
-        <WS
-          ref={ref => {this.ws = ref}}
-          url="wss://148.84.205.240:5005"
-          onOpen={() => {
-            console.log('Open!')
-            this.ws.send('Hello')
-          }}
-          onMessage={console.log}
-          onError={console.log}
-          onClose={console.log}
-          reconnect // Will try to reconnect onClose
-        />
-      </View>
-    );
-  }
+  <Image style={{ backgroundColor: 'red', flex: 1, width: 100, height: 100}} source={{uri: 'http://iconshow.me/media/images/Mixed/Free-Flat-UI-Icons/png/512/eye-24-512.png'}} />
+)}
+  // render() {
+  //   return (
+  //     <View style={styles.container}>
+  //       <View style={styles.header}>
+  //         {/* <Text style={styles.headerText}>A-eye</Text> */}
+  //       </View>
+  //       <View style={[styles.button, {backgroundColor: 'white'}]}>
+  //         <Text style={[styles.text, {fontSize: 40, color: 'black'}]}>{this.state.data}</Text>
+  //       </View>
+  //       <WS
+  //         ref={ref => {this.ws = ref}}
+  //         url="ws://148.84.204.98:8080"
+  //         onOpen={() => {
+  //           console.log('Open!')
+  //           // this.ws.send('====== ARE YOU THIS? =====')
+  //         }}
+  //         onMessage={(resp) => {
+  //           this.convertToSpeech(resp);
+  //         }}
+  //         onError={console.log}
+  //         onClose={console.log}
+  //         reconnect // Will try to reconnect onClose
+  //       />
+  //     </View>
+  //   );
+  // }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  header: {
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 0,
+    width,
+    height: 100,
+  },
+  headerText: {
+    fontSize: 50
   },
   button: {
     backgroundColor: 'red',
     padding: 10,
     borderWidth: 1,
-    borderRadius: 15,
-    borderColor: 'red'
+    borderRadius: 5,
+    borderColor: '#e0e0e0'
   },
-  welcome: {
-    fontSize: 20,
+  text: {
+    color: 'white',
+    fontWeight: '200',
     textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    padding: 20,
+  }
 });
